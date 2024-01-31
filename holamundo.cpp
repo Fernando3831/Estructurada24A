36b191ac0524b6b9fb71f6fb9989dd13c73254d1
Fernando Gomez <juan.gomez9287@alumnos.udg.mx>
2024-01-30 18:02:08 -0600

    Update holamundo.cpp

--- holamundo.cpp
+++ holamundo.cpp
@@ -1,4 +1,11 @@
-/* Juan Fernando Gomez Navarro */
+/* 
+Nombre: Juan Fernando Gomez Navarro
+Fecha: 29 de Enero de 2024
+Programa: Actividad de programación - 1
+CENTRO UNIVERSITARIO DE LOS ALTOS / UNIVERSIDAD DE GUADALAJARA
+INGENIERIA EN COMPUTACION / 2DO SEMESTRE
+PROFESOR: Sergio Franco Casillas
+Descripcion: Es uncodigo que imprime uno de las frases mas conocidas por los programadores: "Hola Mundo". */
 #include <iostream>
 main(){
 printf("Hola Mundo");
